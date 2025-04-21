328165ae6a285881a24e492b8e6cf61b9b3812dd
Sarah Choubane <123517709+287sarah@users.noreply.github.com>
2025-04-21 12:36:38 -0400

    implémentation des routes pour envoyer et recevoir les devoirs

--- routes.php
+++ routes.php
@@ -137,3 +137,15 @@
     delete('/api/delete-devoir/$id', function($id) {
         devoirs::deleteDevoir($id);
     });
+
+    get('/api/get-devoirs-envoyes/$id', function($id) {
+    devoirs::getDevoirsEnvoyes($id);
+    });
+
+    post('/api/share-devoir/$id', function($id) {
+    devoirs::shareDevoir($id);
+    });
+
+    get('/api/get-devoirs-recus/$id', function($id) {
+    devoirs::getDevoirsRecus($id);
+    });
